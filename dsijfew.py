be54c5745c3d6ae4e5e782f02aa3d7f84d52f945
1122-sudo <mazinabdulhadi9@gmail.com>
2025-07-16 11:52:32 +0500

    Add files via upload

---
+++ dsijfew.py
@@ -0,0 +1,3 @@
+file = open('dairy.txt', 'w')
+file.write('i went to the park and saw a dog runing towards me and i also ran away from the dog the i came back to home and wrote this to know u')
+file.close()
